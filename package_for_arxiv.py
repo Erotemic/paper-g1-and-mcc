851cc06040f1e4d744b310a0925ef0af53b7a8fe
joncrall <erotemic@gmail.com>
2023-06-22 18:27:45 -0400

    Improve the proof

---
+++ package_for_arxiv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def main():
+    import pathlib
+    import zipfile
+    dpath = pathlib.Path('.').resolve()
+    dname = dpath.name
+
+    fpaths_rel = [
+        'arxiv.sty',
+        'main.bbl',
+        'main.tex',
+        'references.bib',
+        'orcid.pdf',
+    ]
+    zip_fpath = dname + '.zip'
+    zfile = zipfile.ZipFile(zip_fpath, mode='w',
+                            compression=zipfile.ZIP_DEFLATED, compresslevel=9)
+    with zfile:
+        for fpath_rel in fpaths_rel:
+            print(f'package fpath_rel={fpath_rel}')
+            zfile.write(fpath_rel)
+    print(f'wrote zip_fpath={zip_fpath}')
+
+
+if __name__ == '__main__':
+    main()
